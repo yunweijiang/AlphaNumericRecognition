81ff86d26743fbb569e54569f3670f42aead07a7
Esperanzora <caoliu06011947@hotmail.com>
2015-11-27 08:58:09 -0500

    Create errorActor.scala

---
+++ src/main/scala/edu.neu.coe.scala/errorActor.scala
@@ -0,0 +1,30 @@
+package edu.neu.coe.scala
+import actors._, Actor._
+
+
+case class digitError(number: Int)
+case class getDigitError(sender: Actor)
+case class letterError(number: Int)
+case class getLetterError(sender: Actor)
+case class totalError(number: Int)
+case class getTotalError(sender: Actor)
+
+case class terminate(sender:Actor)
+class errorActor extends Actor
+{
+   override def act(): Unit=process(0,0,0)
+   def process(digit: Int, letter:Int,total:Int):Unit= 
+   {
+       receive
+       {
+         case digitError(number) => process(digit+number,letter,total)
+         case getDigitError(a) =>a ! digit; process(digit,letter,total)
+         case letterError(number) => process(digit,letter+number,total)
+         case getLetterError(a) => a ! letter; process(digit,letter,total)
+         case totalError(number) => process(digit,letter,total+number)
+         case getTotalError(a) => a ! total; process(digit,letter,total)
+
+         case terminate(a) => a.trapExit
+       }
+   }
+}
